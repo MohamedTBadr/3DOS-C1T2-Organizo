b2eee4d9ee7105991a62631f92b4116964291eb0
Salmamohamed33 <140505053+Salmamohamed33@users.noreply.github.com>
2024-09-14 03:31:04 +0300

    Add files via upload

--- ViewSprints.php
+++ ViewSprints.php
@@ -18,21 +18,21 @@ else
     else
         $page=$_GET['page'];
 
-    $num="SELECT * FROM sprint WHERE project_id = $pid";
+    $num="SELECT * FROM `sprint` WHERE `project_id` = $pid";
     $run_num=mysqli_query($connect, $num);
     $num_rows=mysqli_num_rows($run_num);
 
     $total_pages=ceil($num_rows / $limit);
     $offset = ($page - 1) * $limit;
-    // $select="SELECT * FROMsprint limit $limit offset $offset";
+    // $select="SELECT * FROM`sprint` limit $limit offset $offset";
     // $run_select=mysqli_query($connect,$select);
 
-    $memberStmt = "SELECT * FROM project_member WHERE user_id = $user_id AND project_id = $pid";
+    $memberStmt = "SELECT * FROM `project_member` WHERE `user_id` = $user_id AND `project_id` = $pid";
     $ExecMember = mysqli_query($connect, $memberStmt);
     if (mysqli_num_rows($ExecMember) == 0)
         header("Location: index.php"); // not a member = kick
 
-    $sprintsStmt = "SELECT * FROM sprint WHERE project_id = $pid ORDER BY end_date limit $limit offset $offset";
+    $sprintsStmt = "SELECT * FROM `sprint` WHERE `project_id` = $pid ORDER BY `end_date` limit $limit offset $offset";
     $ExecSprint = mysqli_query($connect, $sprintsStmt);
     if (mysqli_num_rows($ExecSprint) == 0)
         $error = "There are no sprints for this Project yet";
@@ -65,7 +65,7 @@ else
         }
         else
         {
-            $AddSprStmt = "INSERT INTO sprint (sprint_name, start_date, end_date, project_id) VALUES ('$sprint_name', '$start_date', '$end_date', '$pid')";
+            $AddSprStmt = "INSERT INTO `sprint` (`sprint_name`, `start_date`, `end_date`, `project_id`) VALUES ('$sprint_name', '$start_date', '$end_date', '$pid')";
             $ExecAddSpr = mysqli_query($connect, $AddSprStmt);
             if ($ExecAddSpr) {
                 $done = "Sprint Successfully Added";
@@ -80,7 +80,7 @@ else
     if (isset($_GET['ds'])) // del logic
     {
         $sprint_id = $_GET['ds'];
-        $DeleteSprStmt = "DELETE FROM sprint WHERE sprint_id = $sprint_id";
+        $DeleteSprStmt = "DELETE FROM `sprint` WHERE `sprint_id` = $sprint_id";
         $ExecDeleteSpr = mysqli_query($connect, $DeleteSprStmt);
         if ($ExecDeleteSpr)
         {
@@ -92,7 +92,7 @@ else
     if (isset($_GET['es'])) //edit logic
     {
         $sprint_id = $_GET['es'];
-        $EditSprStmt = "SELECT * FROM sprint WHERE sprint_id = $sprint_id";
+        $EditSprStmt = "SELECT * FROM `sprint` WHERE `sprint_id` = $sprint_id";
         $ExecEditSpr = mysqli_query($connect, $EditSprStmt);
         $sprintData = mysqli_fetch_assoc($ExecEditSpr);
         if (isset($_POST['update-spr']))
@@ -101,7 +101,7 @@ else
             $start_date = $_POST['start_date'];
             $end_date = $_POST['end_date'];
 
-            $UpdateSprStmt = "UPDATE sprint SET sprint_name = '$sprint_name', start_date = '$start_date', end_date = '$end_date' WHERE sprint_id = $sprint_id";
+            $UpdateSprStmt = "UPDATE `sprint` SET `sprint_name` = '$sprint_name', `start_date` = '$start_date', `end_date` = '$end_date' WHERE `sprint_id` = $sprint_id";
             $ExecUpdateSpr = mysqli_query($connect, $UpdateSprStmt);
             if ($ExecUpdateSpr)
             {
@@ -113,16 +113,16 @@ else
 }
 $select="SELECT * FROM sprint ";
 $run_select=mysqli_query($connect,$select);
-if (isset($_POST['search']) || isset($_POST['text'])) {
-    $text = mysqli_real_escape_string($connect, $_POST['text']);
+if(isset($_POST['search'])){
+    $text=mysqli_real_escape_string($connect, $_POST['text']);
 
-    $select_search="SELECT * FROM sprint WHERE (sprint_name LIKE '%$text%') AND project_id = $pid";
+    $select_search="SELECT * FROM sprint WHERE (sprint_name LIKE '%$text%') AND `project_id` = $pid";
     $run_select_search=mysqli_query($connect,$select_search);
     $num_rows = mysqli_num_rows($run_select_search);
     $total_search_pages = ceil($num_rows / $limit);
     $offset = ($page - 1) * $limit;
 
-    $select_search = "SELECT * FROM sprint WHERE (sprint_name LIKE '%$text%') AND project_id = $pid LIMIT $limit OFFSET $offset";
+    $select_search = "SELECT * FROM sprint WHERE (sprint_name LIKE '%$text%') AND `project_id` = $pid LIMIT $limit OFFSET $offset";
     $run_select_search = mysqli_query($connect, $select_search);
 }
 ?>
@@ -143,7 +143,120 @@ if (isset($_POST['search']) || isset($_POST['text'])) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
               integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
               crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+
+              <style>
+                .warning
+                {
+                    display: none; color: red;
+                    position: static;
+                    width: 65%;
+                    margin: -13% auto -1%;
+                    font-size: 1vw;
+                }
+                .warning.visible {display: block}
+                #s-btn {margin-top: 20px}
+
+                .error {
+                    font-size: 15px;
+                    margin: 2px;
+                    text-align: center;
+                    color: red;
+                    width: 100%;
+                }
+             
+        /* Popup delete styling */
+        .popup-sprint {
+    display: none; /* Hide popups by default */
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 20px;
+    background: white;
+    border: 1px solid #ccc;
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+    z-index: 1000;
+    text-align: center;
+    border-radius: 7px;
+    color:#58151c;
+}
+
+.popup-sprint.show {
+    display: block; /* Show popup when class 'show' is added */
+}
+
+.overlay {
+    display: none; /* Hide overlay by default */
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.5);
+    z-index: 999;
+}
+
+.overlay.show {
+    display: block; /* Show overlay when class 'show' is added */
+}
+
+.lol {
+    color:#58151c;
+}
+
+            </style>
     </head>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function() {
+    $("#searchText").on("input", function() {
+        var searchText = $(this).val();
+        
+        // Prevent search if empty
+        if (searchText === "") {
+            location.reload();  // Reload the page if search is cleared
+            return;
+        }
+        
+        // Perform AJAX request to search
+        $.ajax({
+            url: "ViewSprints.php?pid=<?php echo $pid ?>", // The same URL for the search
+            type: "POST",
+            data: {
+                text: searchText, // Send the search text
+                search: true // Flag to detect the search query
+            },
+            success: function(data) {
+                var results = $(data).find('.pricing-card'); // Extract the updated search results
+                $('.pricing-container').html(results); // Update the UI with new search results
+            }
+        });
+    });
+});
+$('form').on('submit', function(e) {
+    e.preventDefault();  // Prevent the form from submitting in the default way
+    var searchText = $('#searchText').val();
+
+    if (searchText === "") {
+        location.reload();  // Reload page if search is cleared
+        return;
+    }
+
+    $.ajax({
+        url: "ViewSprints.php?pid=<?php echo $pid ?>", // Same page
+        type: "POST",
+        data: { text: searchText, search: true },
+        success: function(data) {
+            var results = $(data).find('.pricing-card');
+            $('.pricing-container').html(results);
+        }
+    });
+});
+
+</script> -->
+
 <body>
     <div class="head">
         <h1>Sprints</h1>
@@ -162,60 +275,71 @@ if (isset($_POST['search']) || isset($_POST['text'])) {
 
         <!-- start first card  -->
         <?php if(isset($_POST['search'])){?>
-        <?php foreach ($run_select_search as $dataa){ ?>
+        <?php foreach ($run_select_search as $data){ ?>
             <div class="pricing-card">
-                <a href="tasks.php?sid=<?php echo $dataa['sprint_id'] ?>">
-                    <div class="d-flex mb-3">
-                        <h2 class="card-title" style="color: #212529 !important;"><?php echo $dataa['sprint_name'] ?></h2>
-                        <div class="ms-4">
-                            <button type="button" class="icon"><a href="editsprints.php?sid=<?php echo $dataa['sprint_id'] ?>"><i class="fa-regular fa-pen-to-square"></i></a></button>
-                            <form method="GET" style="display: inline">
-                                <input type="hidden" name="pid" value="<?php echo $pid; ?>">
-                                <input type="hidden" name="ds" value="<?php echo $dataa['sprint_id']; ?>">
-                                <button type="submit" class="icon"><i class="fa-solid fa-x fs-4" style="color: rgb(151, 2, 2);"></i></button>
+                <div class="d-flex mb-3">
+                    <h2 class="card-title"><?php echo $data['sprint_name'] ?></h2>
+                    <div class="ms-4">
+                        <button type="button" class="icon"><a href="editsprints.php?sid=<?php echo $data['sprint_id'] ?>"><i class="fa-regular fa-pen-to-square"></i></a></button>
+                        
+                        <button type="button" class="icon" onclick="openSprintPopup(<?php echo $data['sprint_id']?>)">
+  <i class="fa-solid fa-x fs-4" style="color: rgb(151, 2, 2);"></i>
+</button>
+
+                            <form method="GET" id="deleteSprintForm-<?php echo $data['sprint_id'];?>" style="display:none;">
+                            <input type="hidden" name="ds" value="<?php echo $data['sprint_id']; ?>">
+                            <input type="hidden" name="pid" value="<?php echo $pid; ?>">
                             </form>
-                        </div>
                     </div>
+                </div>
 
-                    <p class="due-description"><i class="fa-solid fa-clock"></i> Due date: <?php echo date('d M',strtotime($dataa['end_date'])) ?></p>
-                    <?php
-                    $end = strtotime($dataa['end_date']); // sec
-                    $now = time();
-                    $gap = $end - $now;  // sec
-                    $days_left = ceil($gap / 86400); // Tarek, my resolute is still strong!! XD 60.60.24, I think ciel is good; 1 instead of 0 days sounds good
-                    if ($gap < 0) {
-                        $days_left = abs($days_left);
-                        $message = "$days_left days overdue";
-                    } else {
-                        $message = "$days_left days left";
-                    }
 
-                    ?>
-                    <p class="left-description"><i class="fa-solid fa-hourglass-start"></i> <?php echo $message ?></p>
-                    <button class="btnn ms-5">Tasks</button></a>
+
+                <p class="due-description"><i class="fa-solid fa-clock"></i> Due date: <?php echo date('d M',strtotime($data['end_date'])) ?></p>
+                <?php
+                $end = strtotime($data['end_date']); // sec
+                $now = time();
+                $gap = $end - $now;  // sec
+                $days_left = ceil($gap / 86400); // Tarek, my resolute is still strong!! XD 60.60.24, I think ciel is good; 1 instead of 0 days sounds good
+                if ($gap < 0) {
+                    $days_left = abs($days_left);
+                    $message = "$days_left days overdue";
+                } else {
+                    $message = "$days_left days left";
+                }
+
+                ?>
+                <p class="left-description"><i class="fa-solid fa-hourglass-start"></i> <?php echo $message ?></p>
+                <a href="tasks.php?sid=<?php echo $data['sprint_id'] ?>"><button class="btnn ms-5">Tasks</button></a>
             </div>
         <?php } ?>
         <!-- end first card  -->
     </div><!-- end cards container -->
     </a>
+    
     <?php }else{ ?>
 
 
     <!-- start first card  -->
         <?php while ($data = mysqli_fetch_assoc($ExecSprint)) { ?>
             <div class="pricing-card">
-            <a href="tasks.php?sid=<?php echo $data['sprint_id'] ?>">
-                    <div class="d-flex mb-3">
-                        <h2 class="card-title" style="color: #212529 !important;"><?php echo $data['sprint_name'] ?></h2>
+                <div class="d-flex mb-3">
+                    <h2 class="card-title"><?php echo $data['sprint_name'] ?></h2>
                     <div class="ms-4">
                         <button type="button" class="icon"><a href="editsprints.php?sid=<?php echo $data['sprint_id'] ?>"><i class="fa-regular fa-pen-to-square"></i></a></button>
-                        <form method="GET" style="display: inline">
-                            <input type="hidden" name="pid" value="<?php echo $pid; ?>">
+                        
+                        <button type="button" class="icon" onclick="openSprintPopup(<?php echo $data['sprint_id']?>)">
+  <i class="fa-solid fa-x fs-4" style="color: rgb(151, 2, 2);"></i>
+</button>
+
+                            <form method="GET" id="deleteSprintForm-<?php echo $data['sprint_id'];?>" style="display:none;">
                             <input type="hidden" name="ds" value="<?php echo $data['sprint_id']; ?>">
-                            <button type="submit" class="icon"><i class="fa-solid fa-x fs-4" style="color: rgb(151, 2, 2);"></i></button>
-                        </form>
+                            <input type="hidden" name="pid" value="<?php echo $pid; ?>">
+                            </form>
                     </div>
                 </div>
+
+
 
                 <p class="due-description"><i class="fa-solid fa-clock"></i> Due date: <?php echo date('d M',strtotime($data['end_date'])) ?></p>
                 <?php
@@ -278,21 +402,23 @@ if (isset($_POST['search']) || isset($_POST['text'])) {
             <div class="from-wraapper  Sign-in">
 
 
-                <form method="POST">
-                    <h2>Add sprint</h2>
+        <form method="POST" onsubmit="return validateForm()">
+            <h2>Add sprint</h2>
 
-                    <div class="input-group">
-                        <input type="text" id="task-name" name="sprint_name"  required>
-                        <label for="" id="task-name">sprint name</label>
-                    </div>
+            <div class="input-group">
+                <input type="text" id="sprint_name" name="sprint_name"  oninput="validateName()">
+                <label for="" id="task-name">sprint name</label>
+                <span name="nameError" id="nameError" class="error" style="display:none;"></span>
+            </div>
             </div>
             <div class="input-group">
                 <input type="date" name="start_date" value="<?php echo date('Y-m-d')?>">
                 <label for="">Start Date</label>
             </div>
             <div class="input-group">
-                <input type="date" name="end_date">
+                <input type="date" name="end_date" id="end_date" oninput="validateDate()">
                 <label for="">End Date</label>
+                <span name="dateError" id="dateError" class="error" style="display:none;"></span>
             </div>
             <div class="end d-lg-flex">
                 <button type="submit" class="btnn me-2" name="add-spr">Submit</button>
@@ -339,15 +465,16 @@ if (isset($_POST['search']) || isset($_POST['text'])) {
 
     <!-- delete popup  -->
     <?php if($role_id == 1) {?>
-        <div class="popup alert alert-danger w-75" id="popup3" role="alert">
-            <h2>Are you sure that you want to delete this sprint?</h2>
-            <form method="GET" action="ViewSprints.php">
-                <input type="hidden" name="pid" value="<?php echo $pid; ?>">
-                <input type="hidden" name="ds" value="<?php echo $data['sprint_id']?>">
-                <button type="submit" class="btn btn-outline-danger">Yes</button>
-                <button type="button" class="btn btn-outline-success" onclick="closepopup3()">No</button>
-            </form>
-        </div>
+
+        <!-- Example delete popup HTML -->
+        <div class="popup-sprint" id="popup-sprint-<?php echo $data['sprint_id']; ?>" role="alert">
+    <h3><i class="fa-solid fa-triangle-exclamation"></i>Are you sure you want to delete this task?</h3>
+    <button type="button" class="lol btn btn-outline-dark" onclick="confirmSprintDelete()">Yes</button>
+    <button type="button" class="lol btn btn-outline-dark" onclick="closeSprintPopup()">No</button>
+</div>
+
+
+
     <?php } else if ($role_id == 2) { ?>
         <div class="popup alert alert-danger w-75" id="popup3" role="alert">
             <h2>Only the Leader can delete sprints</h2>
@@ -355,57 +482,32 @@ if (isset($_POST['search']) || isset($_POST['text'])) {
         </div>
     <?php } ?>
     <script src="js/sprints.js"></script>
+    <script src="js/validation.js"></script>
+    <script>
+let deleteSprintId;
+function openSprintPopup(sprintId) {
+    console.log('Opening popup for sprintId:', sprintId);
+    deleteSprintId = sprintId;
+    document.getElementById('popup-sprint-' + sprintId).classList.add('show');
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
+}
 
-$(document).ready(function() {
-    $("#searchText").on("input", function() {
-        var searchText = $(this).val();
-        
-        // Prevent search if empty
-        if (searchText === "") {
-            location.reload();  // Reload the page if search is cleared
-            return;
-        }
-        
-        // Perform AJAX request to search
-        $.ajax({
-            url: "ViewSprints.php?pid=<?php echo $pid ?>", // The same URL for the search
-            type: "POST",
-            data: {
-                text: searchText, // Send the search text
-                search: true // Flag to detect the search query
-            },
-            success: function(data) {
-                var results = $(data).find('.pricing-card'); // Extract the updated search results
-                $('.pricing-container').html(results); // Update the UI with new search results
-            }
-        });
-    });
-});
-$('form').on('submit', function(e) {
-    e.preventDefault();  // Prevent the form from submitting in the default way
-    var searchText = $('#searchText').val();
+function closeSprintPopup() {
+    console.log('Closing popup for sprintId:', deleteSprintId);
+    document.getElementById('popup-sprint-' + deleteSprintId).classList.remove('show');
 
-    if (searchText === "") {
-        location.reload();  // Reload page if search is cleared
-        return;
-    }
+}
 
-    $.ajax({
-        url: "ViewSprints.php?pid=<?php echo $pid ?>", // Same page
-        type: "POST",
-        data: { text: searchText, search: true },
-        success: function(data) {
-            var results = $(data).find('.pricing-card');
-            $('.pricing-container').html(results);
-        }
-    });
-});
+function confirmSprintDelete() {
+    console.log('Confirming delete for sprintId:', deleteSprintId);
+    document.getElementById('deleteSprintForm-' + deleteSprintId).submit();
+}
+
+
 
 </script>
+
 </body>
 </html>
 
-<!-- $select_search="SELECT * FROM sprint WHERE project_id = $pid &&  (sprint_name LIKE '%$text%')";-->
+<!-- $select_search="SELECT * FROM sprint WHERE `project_id` = $pid &&  (sprint_name LIKE '%$text%')";-->
