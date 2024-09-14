8a4b87cdddcca58aaa9f4091237f3730bdd2798d
alaa <alaanaguib96@gmail.com>
2024-09-14 07:50:55 +0300

    Update editMessage.php

--- app/ajax/editMessage.php
+++ app/ajax/editMessage.php
@@ -7,7 +7,10 @@ if (isset($_POST['chat_id']) && isset($_POST['new_message'])) {
     $message_id = $_POST['chat_id'];
     $new_message = $_POST['new_message'];
 
-    $edit_message = "UPDATE `chats` SET `message` = '$new_message' WHERE `chat_id` = '$message_id'";
+    $edit_message = "UPDATE `chats` 
+                    SET `message` = '$new_message',
+                        `edited` = '1'
+                    WHERE `chat_id` = '$message_id'";
     $run_edit_message = mysqli_query($connect, $edit_message);
 
     if ($run_edit_message) {
