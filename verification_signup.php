d5e00099ed9e08ff1e6bc45a1230f8a58c1b88e9
ShehabSerry <111205286+ShehabSerry@users.noreply.github.com>
2024-09-15 14:14:32 +0300

    Alaa Joindate + revert otp conditions

--- verification_signup.php
+++ verification_signup.php
@@ -26,50 +26,53 @@ if(isset($_POST['submit']))
         $otp= $_POST['otp1']."".$_POST['otp2']."".$_POST['otp3']."".$_POST['otp4']."".$_POST['otp5'];
         $current_time=time();
 
-        if($current_time - $old_time > 60) // WE HAVE ONE MINUTE TO VERIFY OTP - could be less FRONT - BACK DECIDE
+        if($rand == $otp)
         {
-            unset($_SESSION['otp']);
-            $error="Expired OTP";
-        }
-        elseif($rand==$otp)
-        {
-            $massage1=" 
-            <body style='font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #fffffa; color: #00000a;'>
-                <div style='background-color: #0a7273; padding: 20px; text-align: center; color: #fffffa;'>
-                    <h1>Welcome to Organizo, <span style='color: #fda521;'>$first_name</span>!</h1>
-                </div>
-                <div style='padding: 20px; background-color: #fffffa; color: #00000a;'>
-                    <p style='color: #00000a;'>Dear <span style='color: #fda521;'>$first_name $last_name</span>,</p>
-                    <p style='color: #00000a;'>Thank you for joining Organizo! We are thrilled to have you on board.</p>
-                    <p style='color: #00000a;'>Here are some things you can do to get started:</p>
-                    <ul>
-                        <li style='color: #00000a'>Explore our features and tools to manage your tasks efficiently.</li>
-                        <li style='color: #00000a'>Customize your profile to make it your own.</li>
-                        <li style='color: #00000a'>Connect with other users and share your experiences.</li>
-                    </ul>
-                    <p style='color: #00000a;'>If you have any questions or need assistance, feel free to reach out to our support team at any time.</p>
-                    <p style='color: #fda521;'>Happy tasking!</p>
-                    <p style='color: #00000a;'>Best regards,<br>The Organizo Team</p>
-                </div>
-                <div style='background-color: #0a7273; padding: 10px; text-align: center; color: #fffffa;'>
-                    <p style='color: #fffffa;'>For support and updates, please visit our website or contact us via email.</p>
-                    <p style='color: #fffffa;'>Email: <a href='mailto:organizohelp@gmail.com' style='color: #fda521;'>organizohelp@gmail.com</a></p>
-                </div>
-            </body>
+            if($current_time - $old_time > 60) // WE HAVE ONE MINUTE TO VERIFY OTP - could be less FRONT - BACK DECIDE
+            {
+                //unset($_SESSION['otp']);
+                $error="Expired OTP";
+            }
+            else
+            {
+                $massage1=" 
+                <body style='font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #fffffa; color: #00000a;'>
+                    <div style='background-color: #0a7273; padding: 20px; text-align: center; color: #fffffa;'>
+                        <h1>Welcome to Organizo, <span style='color: #fda521;'>$first_name</span>!</h1>
+                    </div>
+                    <div style='padding: 20px; background-color: #fffffa; color: #00000a;'>
+                        <p style='color: #00000a;'>Dear <span style='color: #fda521;'>$first_name $last_name</span>,</p>
+                        <p style='color: #00000a;'>Thank you for joining Organizo! We are thrilled to have you on board.</p>
+                        <p style='color: #00000a;'>Here are some things you can do to get started:</p>
+                        <ul>
+                            <li style='color: #00000a'>Explore our features and tools to manage your tasks efficiently.</li>
+                            <li style='color: #00000a'>Customize your profile to make it your own.</li>
+                            <li style='color: #00000a'>Connect with other users and share your experiences.</li>
+                        </ul>
+                        <p style='color: #00000a;'>If you have any questions or need assistance, feel free to reach out to our support team at any time.</p>
+                        <p style='color: #fda521;'>Happy tasking!</p>
+                        <p style='color: #00000a;'>Best regards,<br>The Organizo Team</p>
+                    </div>
+                    <div style='background-color: #0a7273; padding: 10px; text-align: center; color: #fffffa;'>
+                        <p style='color: #fffffa;'>For support and updates, please visit our website or contact us via email.</p>
+                        <p style='color: #fffffa;'>Email: <a href='mailto:organizohelp@gmail.com' style='color: #fda521;'>organizohelp@gmail.com</a></p>
+                    </div>
+                </body>
                     ";
-            unset($_SESSION['otp']); // not to ruin forgot password I guess
+                unset($_SESSION['otp']); // not to ruin forgot password I guess
 
-            $mail->setFrom('organizohelp@gmail.com', 'Organizo');
-            $mail->addAddress($email);
-            $mail->isHTML(true);
-            $mail->Subject = 'Welcome Aboard';
-            $mail->Body=($massage1);
-            $mail->send();
+                $mail->setFrom('organizohelp@gmail.com', 'Organizo');
+                $mail->addAddress($email);
+                $mail->isHTML(true);
+                $mail->Subject = 'Welcome Aboard';
+                $mail->Body=($massage1);
+                $mail->send();
 
-            $insert="INSERT INTO `user` VALUES (NULL,'$first_name','$last_name','$hash_password','$email','$phone',1,2, 'defaultprofile.png', '$timestamp','$join_date')";
-            $run_insert=mysqli_query($connect,$insert);
-            $_SESSION['logCHK'] = true;
-            header("location:login.php");
+                $insert="INSERT INTO `user` VALUES (NULL,'$first_name','$last_name','$hash_password','$email','$phone',1,2, 'defaultprofile.png', '$timestamp','$join_date')";
+                $run_insert=mysqli_query($connect,$insert);
+                $_SESSION['logCHK'] = true;
+                header("location:login.php");
+            }
         }
         else
             $error= "Incorrect OTP";
