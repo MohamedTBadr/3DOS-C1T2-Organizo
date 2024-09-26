a870513bdcc31f69434e9d4b74fdc2f912701795
lailagabr <124739171+lailagabr@users.noreply.github.com>
2024-09-25 20:13:33 -0700

    Add files via upload

--- admin/admin_profile.php
+++ admin/admin_profile.php
@@ -14,72 +14,81 @@ if (isset($fetch) && !is_null($fetch)) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/adminprofile.css">
-    <link href="../img/keklogo.png" rel="icon">
-    <title>Admin profile</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/adminprofile.css">
+  <link href="../img/keklogo.png" rel="icon">
+  <title>Admin profile</title>
 </head>
 
 <body>
-    <div class="container">
-      <div class="header-container">
-        <div class="header">
-          <h1 class="main-heading"><?php echo $name ;?></h1>
-          <div class="stats">
-            <span class="stat-module">
-              E-mail: <span class="stat-number"><?php echo $email;?>
-            </span>
-            
-          </div>
-        </div> <!-- END header -->
-      </div>
-        
-      <div class="overlay-header"></div>
-      
-      <div class="body">
-        <div class="u-clearfix"></div>
-        <div class="body-info">
+  <div class="container">
+    <div class="header-container">
+      <div class="header">
+        <h1 class="main-heading">
+          Name :
+          <?php echo $name ;?>
+        </h1>
+        <div class="stats">
+            <h2 class="main-heading">
+              
+            E-mail: 
+              <?php echo $email;?>
+</h2>
+        </div>
+      </div> <!-- END header -->
+    </div>
+    <div class="body">
+      <div class="u-clearfix"></div>
+      <div class="body-info">
         <a href="Edit password.php">
           <button class="c-button c-button--gooey" type="submit"> Update password
             <div class="c-button__blobs">
-            <div></div>
-            <div></div>
-            <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
             </div>
           </button>
-          </a>
-          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="display: block; height: 0; width: 0;">
-            <defs>
-              <filter id="goo">
-                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"></feGaussianBlur>
-                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo"></feColorMatrix>
-                <feBlend in="SourceGraphic" in2="goo"></feBlend>
-              </filter>
-            </defs>
-          </svg>
-          <form method="post" >
+        </a>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="display: block; height: 0; width: 0;">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"></feGaussianBlur>
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo">
+              </feColorMatrix>
+              <feBlend in="SourceGraphic" in2="goo"></feBlend>
+            </filter>
+          </defs>
+        </svg>
+        <form method="post">
           <button class="c-button c-button--gooey" type="submit" name="logout">Logout
-              <div class="c-button__blobs">
+            <div class="c-button__blobs">
               <div></div>
               <div></div>
               <div></div>
-              </div>
-            </button>
-            </form>
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="display: block; height: 0; width: 0;">
-              <defs>
-                <filter id="goo">
-                  <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"></feGaussianBlur>
-                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo"></feColorMatrix>
-                  <feBlend in="SourceGraphic" in2="goo"></feBlend>
-                </filter>
-              </defs>
-            </svg>
-        </div>
+            </div>
+          </button>
+        </form>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="display: block; height: 0; width: 0;">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"></feGaussianBlur>
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo">
+              </feColorMatrix>
+              <feBlend in="SourceGraphic" in2="goo"></feBlend>
+            </filter>
+          </defs>
+        </svg>
       </div>
+    </div>
+    <div class="overlay-header"></div>
+
+
+    <div class="bodyy">
     </div>
   </div>
 </body>
+
 </html>
